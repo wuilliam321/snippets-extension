1ccf9efaad6c12d98d059d407ed258a321719b7c
Carlos Araque <cararqueu@gmail.com>
2021-03-21 18:26:34 -0300

    Co-authored-by: Wuilliam Lacruz <wuilliam321@gmail.com>

--- src/popup.js
+++ src/popup.js
@@ -4,15 +4,21 @@ const formLogin = document.getElementById("form-login")
 
 formLogin.addEventListener("submit", doLogin)
 
-function doLogin() {
+function doLogin(event) {
+    event.preventDefault();
+
     const inputEmail = document.getElementById("email");
     const inputPassword = document.getElementById("password");
-    if (!main.isFormValid(inputEmail.value, inputPassword.value)) {
-      // mostrar que el formulario esta invalido "Incorrect email or password"
+    const formError = document.getElementById("form-error");
+
+    if (main.isFormValid(inputEmail.value, inputPassword.value)) {
+        // hacer POST al login
+        // guardar el token en la pc
+        // mostrar el dashboard con el boton logout, (no quiero el form)
+        // mostrar un error si no pudo hacerse el login correctament
+        formError.style.display = 'none';
     } else {
-      // hacer POST al login
-      // guardar el token en la pc
-      // mostrar el dashboard con el boton logout, (no quiero el form)
-      // mostrar un error si no pudo hacerse el login correctament
+        // mostrar que el formulario esta invalido "Incorrect email or password"
+        formError.style.display = 'block';
     }
 }

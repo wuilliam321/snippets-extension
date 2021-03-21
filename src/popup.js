eb8943c447f01f81ce7df13d26f42b479823a74a
Wuilliam Lacruz <wuilliam.lacruz@pedidosya.com>
2021-03-21 17:04:23 -0300

    added steps to achieve

--- src/popup.js
+++ src/popup.js
@@ -7,5 +7,12 @@ formLogin.addEventListener("submit", doLogin)
 function doLogin() {
     const inputEmail = document.getElementById("email");
     const inputPassword = document.getElementById("password");
-    main.isFormValid(inputEmail.value, inputPassword.value);
+    if (!main.isFormValid(inputEmail.value, inputPassword.value)) {
+      // mostrar que el formulario esta invalido "Incorrect email or password"
+    } else {
+      // hacer POST al login
+      // guardar el token en la pc
+      // mostrar el dashboard con el boton logout, (no quiero el form)
+      // mostrar un error si no pudo hacerse el login correctament
+    }
 }

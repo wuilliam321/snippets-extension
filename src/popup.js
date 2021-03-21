24377c6bc175db11a9009e18e29885ac5d35afd8
Carlos Araque <cararqueu@gmail.com>
2021-03-21 19:16:14 -0300

    http request to login

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const main = require('./main');
 
 const formLogin = document.getElementById("form-login")
@@ -12,10 +13,29 @@ function doLogin(event) {
     const formError = document.getElementById("form-error");
 
     if (main.isFormValid(inputEmail.value, inputPassword.value)) {
+        const Url = 'https://app.capijzo.com/api/auth/login';
         // hacer POST al login
-        // guardar el token en la pc
-        // mostrar el dashboard con el boton logout, (no quiero el form)
-        // mostrar un error si no pudo hacerse el login correctament
+        axios({
+                method: 'POST',
+                url: Url,
+                data: {
+                    email: inputEmail.value,
+                    password: inputPassword.value
+                }
+            })
+            .then(data => {
+                // guardar el token en la pc
+                // mostrar el dashboard con el boton logout, (no quiero el form)
+                console.log(data);
+                chrome.storage.sync.set({ "data": data.data.access_token }, () => {
+                    console.log(data.data.access_token);
+                });
+            })
+            .catch(err => {
+                // mostrar un error si no pudo hacerse el login correctament
+                console.log(err)
+                formError.style.display = 'block'
+            });
         formError.style.display = 'none';
     } else {
         // mostrar que el formulario esta invalido "Incorrect email or password"

2a7810d006fd1d06a5a54ab85011358b7af9ac25
Wuilliam Lacruz <wuilliam.lacruz@pedidosya.com>
2021-03-22 19:07:03 -0300

    test for login requests

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 function isFormValid(email, password) {
   const isValid = isValidEmail(email) && isValidPassword(password);
   return isValid;
@@ -17,8 +19,31 @@ function isValidPassword(password) {
   return false;
 }
 
+async function login(email, password) {
+  try {
+    const url = 'https://app.capijzo.com/api/auth/login';
+    const { data } = await axios.post(url, {
+      email: email,
+      password: password,
+    });
+    return data;
+  } catch (err) {
+    let error = {
+      status: 'unknown',
+      data: {
+        message: 'Unknown',
+      },
+    };
+    if (err && err.response) {
+      error = err.response;
+    }
+    throw error;
+  }
+}
+
 export default {
   isFormValid: isFormValid,
   isValidEmail: isValidEmail,
   isValidPassword: isValidPassword,
+  login: login,
 };
--- src/popup.js
+++ src/popup.js
@@ -1,44 +1,35 @@
-import axios from 'axios';
 import main from './main';
 
 const formLogin = document.getElementById('form-login');
 
 formLogin.addEventListener('submit', doLogin);
 
-function doLogin(event) {
+async function doLogin(event) {
   event.preventDefault();
 
   const inputEmail = document.getElementById('email');
   const inputPassword = document.getElementById('password');
   const formError = document.getElementById('form-error');
 
-  if (main.isFormValid(inputEmail.value, inputPassword.value)) {
-    const Url = 'https://app.capijzo.com/api/auth/login';
-    // hacer POST al login
-    axios({
-      method: 'POST',
-      url: Url,
-      data: {
-        email: inputEmail.value,
-        password: inputPassword.value,
-      },
-    })
-      .then((data) => {
-        // guardar el token en la pc
-        // mostrar el dashboard con el boton logout, (no quiero el form)
-        console.log(data);
-        chrome.storage.sync.set({ data: data.data.access_token }, () => {
-          console.log(data.data.access_token);
-        });
-      })
-      .catch((err) => {
-        // mostrar un error si no pudo hacerse el login correctament
-        console.log(err);
-        formError.style.display = 'block';
-      });
-    formError.style.display = 'none';
-  } else {
-    // mostrar que el formulario esta invalido "Incorrect email or password"
+  formError.style.display = 'none';
+
+  if (!main.isFormValid(inputEmail.value, inputPassword.value)) {
+    formError.style.display = 'block';
+    return;
+  }
+
+  // hacer POST al login
+  try {
+    const data = await main.login(inputEmail.value, inputPassword.value);
+    // mostrar el dashboard con el boton logout, (no quiero el form)
+    console.log('success', data);
+
+    // guardar el token en la pc
+    // chrome.storage.sync.set({ data: data.data.access_token }, () => {
+    //   console.log(data.data.access_token);
+    // });
+  } catch (err) {
+    console.log('error', err.status);
     formError.style.display = 'block';
   }
 }

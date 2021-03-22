562f2f050ad952033c6ca018d1925f9c507e8a12
Wuilliam Lacruz <wuilliam.lacruz@pedidosya.com>
2021-03-22 16:29:16 -0300

    change all the code by using prettier

--- __tests__/main.js
+++ __tests__/main.js
@@ -11,46 +11,46 @@ import main from '../src/main';
 // });
 
 describe('Email Form Validation', () => {
-    test('given an empty email, should return false', () => {
-        const result = main.isFormValid('', '12345');
-        expect(result).toBe(false);
-    });
+  test('given an empty email, should return false', () => {
+    const result = main.isFormValid('', '12345');
+    expect(result).toBe(false);
+  });
 
-    test('given an valid email, should return true', () => {
-        const result = main.isFormValid('a@a.com', '12345');
-        expect(result).toBe(true);
-    });
+  test('given an valid email, should return true', () => {
+    const result = main.isFormValid('a@a.com', '12345');
+    expect(result).toBe(true);
+  });
 
-    test('given an invalid email, should return false', () => {
-        const result = main.isFormValid('wuilliam', '12345');
-        expect(result).toBe(false);
-    });
+  test('given an invalid email, should return false', () => {
+    const result = main.isFormValid('wuilliam', '12345');
+    expect(result).toBe(false);
+  });
 
-    // Addional Validation Tests
-    test('given an invalid email without user, should return false', () => {
-        const result = main.isFormValid('@algo.com', '12345');
-        expect(result).toBe(false);
-    });
+  // Addional Validation Tests
+  test('given an invalid email without user, should return false', () => {
+    const result = main.isFormValid('@algo.com', '12345');
+    expect(result).toBe(false);
+  });
 
-    test('given an invalid email without domain, should return false', () => {
-        const result = main.isFormValid('algo@.com', '12345');
-        expect(result).toBe(false);
-    });
+  test('given an invalid email without domain, should return false', () => {
+    const result = main.isFormValid('algo@.com', '12345');
+    expect(result).toBe(false);
+  });
 
-    test('given an invalid email with spaces, should return false', () => {
-        const result = main.isFormValid('algo espacio@.com', '12345');
-        expect(result).toBe(false);
-    });
+  test('given an invalid email with spaces, should return false', () => {
+    const result = main.isFormValid('algo espacio@.com', '12345');
+    expect(result).toBe(false);
+  });
 });
 
 describe('Pasword Form Validation', () => {
-    test('given an empty password, should return false', () => {
-        const result = main.isFormValid('a@a.com', '');
-        expect(result).toBe(false);
-    });
+  test('given an empty password, should return false', () => {
+    const result = main.isFormValid('a@a.com', '');
+    expect(result).toBe(false);
+  });
 
-    test('given a good password and email, should return true', () => {
-        const result = main.isFormValid('a@a.com', 'aoeu');
-        expect(result).toBe(true);
-    });
+  test('given a good password and email, should return true', () => {
+    const result = main.isFormValid('a@a.com', 'aoeu');
+    expect(result).toBe(true);
+  });
 });

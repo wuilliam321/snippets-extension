045adb64d2b2fdc999f194170daa4743c1c4685c
Wuilliam Lacruz <wuilliam.lacruz@pedidosya.com>
2021-03-24 18:37:23 -0300

    start with parser

--- __tests__/contentScript.js
+++
@@ -1,8 +0,0 @@
-import parser from '../src/core/parser';
-
-describe('Parser', () => {
-  test('parseHtml should return empty if nothing passed', () => {
-    const result = parser.parseHtml();
-    expect(result).toBe('');
-  });
-});
--- src/core/parser.js
+++ src/core/parser.js
@@ -1,3 +1,11 @@
-export default {
-  parseHtml: () => {console.log('hello from parser');}
+function parseHtml(content) {
+  if (!content) {
+    return '';
+  }
+  const el = document.createElement('template');
+  el.innerHTML = content;
+  return el.content;
 }
+export default {
+  parseHtml,
+};

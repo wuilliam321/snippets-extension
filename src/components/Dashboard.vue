28c5659fe566bae6eb0b40dcff3ed033f6274aa7
Carlos Araque <cararqueu@gmail.com>
2021-03-24 00:07:03 -0300

    Co-authored-by: Wuilliam Lacruz <wuilliam321@gmail.com>

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -7,7 +7,7 @@
   <!--   Typing Speed -->
   <!--   Inserted Words -->
   <!-- Footer -->
-  <router-link to="/">Log out</router-link>
+  <a href="#" @click="doLogout">Log out</a>
 </template>
 
 <script>
@@ -17,6 +17,9 @@ export default Vue.extend({
   methods: {
     async doLogout(event) {
       event.preventDefault();
+      chrome.storage.sync.remove('access_token', () => {
+        this.$router.push('/');
+      });
 
       // Here will go the logout process
       // Remove token

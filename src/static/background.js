3bc2de2ec1801d7a6649265005bdaf38efd23901
Wuilliam Lacruz <wuilliam.lacruz@pedidosya.com>
2021-03-24 17:26:55 -0300

    background and content script are built with parcel now

--- src/static/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Welcome');
-});
-// console.log('bac.js');
-// chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-//     console.log("Received %o from %o, frame", msg, sender.tab, sender.frameId);
-//     sendResponse("esto es desde bg");
-// });

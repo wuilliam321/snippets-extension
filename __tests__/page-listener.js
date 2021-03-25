fad7bba5b191001059d7e0adaddc905163c9e3de
Wuilliam Lacruz <wuilliam.lacruz@pedidosya.com>
2021-03-25 19:00:33 -0300

    better settings

--- __tests__/page-listener.js
+++ __tests__/page-listener.js
@@ -1,5 +1,85 @@
 import pageListener from '../src/core/page-listener';
 import parser from '../src/core/parser';
+import settings from '../src/core/settings';
+
+const allSnippets = [
+  {
+    id: 3,
+    name: 'Greetings Mail',
+    shortcode: 'aa',
+    text:
+      '<p><s>Strike</s></p><p><br></p><h1>Header 1</h1><ul><li>List</li><li class="ql-indent-1">List Padding</li></ul>',
+    category_id: 62,
+    pivot: {
+      user_id: 57,
+      item_id: 73,
+      item_type: 'App\\Models\\Snippet',
+      scope: 1,
+      approved: 1,
+    },
+    category: {
+      id: 62,
+      name: 'test',
+      color_id: 5,
+      created_at: '2021-03-24T03:29:35.000000Z',
+      updated_at: '2021-03-24T03:29:35.000000Z',
+      color: { id: 5, hex: '#0077B6' },
+    },
+    labels: [],
+    users: [],
+  },
+  {
+    id: 2,
+    name: 'Greetings Mail',
+    shortcode: 'bb',
+    text:
+      '<p><s>Strike</s></p><p><br></p><h1>Header 2</h1><ul><li>List</li><li class="ql-indent-1">List Padding</li></ul>',
+    category_id: 62,
+    pivot: {
+      user_id: 57,
+      item_id: 73,
+      item_type: 'App\\Models\\Snippet',
+      scope: 1,
+      approved: 1,
+    },
+    category: {
+      id: 62,
+      name: 'test',
+      color_id: 5,
+      created_at: '2021-03-24T03:29:35.000000Z',
+      updated_at: '2021-03-24T03:29:35.000000Z',
+      color: { id: 5, hex: '#0077B6' },
+    },
+    labels: [],
+    users: [],
+  },
+  {
+    id: 1,
+    name: 'Greetings Mail',
+    shortcode: 'aaa',
+    text:
+      '<p><s>Strike</s></p><p><br></p><h1>Header 1</h1><ul><li>List</li><li class="ql-indent-1">List Padding</li></ul>',
+    category_id: 62,
+    pivot: {
+      user_id: 57,
+      item_id: 73,
+      item_type: 'App\\Models\\Snippet',
+      scope: 1,
+      approved: 1,
+    },
+    category: {
+      id: 62,
+      name: 'test',
+      color_id: 5,
+      created_at: '2021-03-24T03:29:35.000000Z',
+      updated_at: '2021-03-24T03:29:35.000000Z',
+      color: { id: 5, hex: '#0077B6' },
+    },
+    labels: [],
+    users: [],
+  },
+];
+
 
 describe('Detect shortcode', () => {
   test('on `a` pressed isTriggerKey() should false', () => {
@@ -79,7 +159,8 @@ describe('Detect shortcode', () => {
   });
 });
 describe('Replacement', () => {
-  test('given shorcode triggered replace textarea with parsed html to text', () => {
+  test('given shorcode aa/ triggered replace textarea with parsed html to text', () => {
+    settings.setSnippets(allSnippets);
     const listener = pageListener.PageListener();
     const elem = document.createElement('textarea');
     elem.value = 'aa/'; // workaround to set a default text (while I can make it dynamically)
@@ -91,7 +172,7 @@ describe('Replacement', () => {
     expect(elem.value).toBe(`Strike\n\n\nHeader 1\n * List\n * List Padding`);
   });
 
-  test('given shorcode triggered replace textarea with parsed html to text', () => {
+  test('given shorcode bb/ triggered replace textarea with parsed html to text', () => {
     const listener = pageListener.PageListener();
     const elem = document.createElement('textarea');
     elem.value = 'bb/'; // workaround to set a default text (while I can make it dynamically)
@@ -100,7 +181,7 @@ describe('Replacement', () => {
     elem.dispatchEvent(new KeyboardEvent('keyup', { key: 'b' }));
     elem.dispatchEvent(new KeyboardEvent('keyup', { key: '/' }));
     expect(listener.isTriggerKey()).toBe(true);
-    expect(elem.value).toBe(`Strike\n\nHeader 2\n\n * List\n * List Padding`);
+    expect(elem.value).toBe(`Strike\n\n\nHeader 2\n * List\n * List Padding`);
   });
 
 });
--- src/core/settings.js
+++ src/core/settings.js
@@ -1,68 +1,36 @@
 function Settings() {
-  const snippets = [
-    {
-      id: 74,
-      name: 'Greetings Mail',
-      shortcode: 'bb',
-      text:
-        '<p><s>Strike</s></p><p><br></p><h1>Header 2</h1><ul><li>List</li><li class="ql-indent-1">List Padding</li></ul>',
-      category_id: 62,
-      pivot: {
-        user_id: 57,
-        item_id: 73,
-        item_type: 'App\\Models\\Snippet',
-        scope: 1,
-        approved: 1,
-      },
-      category: {
-        id: 62,
-        name: 'test',
-        color_id: 5,
-        created_at: '2021-03-24T03:29:35.000000Z',
-        updated_at: '2021-03-24T03:29:35.000000Z',
-        color: { id: 5, hex: '#0077B6' },
-      },
-      labels: [],
-      users: [],
-    },
-    {
-      id: 73,
-      name: 'Greetings Mail',
-      shortcode: 'aa',
-      text:
-        '<p><s>Strike</s></p><p><br></p><h1>Header 1</h1><ul><li>List</li><li class="ql-indent-1">List Padding</li></ul>',
-      category_id: 62,
-      pivot: {
-        user_id: 57,
-        item_id: 73,
-        item_type: 'App\\Models\\Snippet',
-        scope: 1,
-        approved: 1,
-      },
-      category: {
-        id: 62,
-        name: 'test',
-        color_id: 5,
-        created_at: '2021-03-24T03:29:35.000000Z',
-        updated_at: '2021-03-24T03:29:35.000000Z',
-        color: { id: 5, hex: '#0077B6' },
-      },
-      labels: [],
-      users: [],
-    },
-  ];
+  let allSnippets = [];
 
   const getSnippetByShortcode = (shortcode) => {
-    // const snippet = snippets.find(s => s.shortcode === shortcode);
-    // console.log(snippets.filter(s => s.shortcode === 'aa'));
-    // console.log(shortcode);
-    // return snippet;
-    return -1;
+    if (!shortcode) {
+      return -1;
+    }
+
+    const snippet = allSnippets.find((s) => s.shortcode === shortcode);
+
+    if (!snippet) {
+      return -1;
+    }
+
+    return snippet;
+  };
+
+  const setSnippets = (snippets) => {
+    allSnippets = [];
+    if (snippets && snippets.length) {
+      allSnippets = snippets;
+    }
+    return allSnippets;
+  };
+
+  const getSnippets = () => {
+    return allSnippets;
   };
 
   return {
-    snippets,
-    getSnippetByShortcode
+    getSnippetByShortcode,
+    setSnippets,
+    getSnippets,
   };
 }
 

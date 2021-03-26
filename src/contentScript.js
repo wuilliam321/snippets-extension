5fd51cf0c93baa5f69bfc590f2c58ab7220fc543
Carlos Araque <cararqueu@gmail.com>
2021-03-25 21:51:55 -0300

    we try to implementation the parser

--- src/contentScript.js
+++ src/contentScript.js
@@ -3,22 +3,18 @@ import pageListener from '../src/core/page-listener';
 
 const listener = pageListener.PageListener();
 
-const shortcode = 'grt';
-let stream = '';
-const trigger_key = '/';
 
 // TODO aqui voy tratando de capas pasar aqui un evet con callback y en el callback hacer lo que quiera con lo que consigo??? maybe???
 document.addEventListener('keyup', (event) => {
-  listener.onKeyPressed(event);
-  console.log('key pressed 1', event);
-  console.log(listener.isTriggerKey(), listener.shortcode(), listener.shortcode() === shortcode);
-  if (listener.isTriggerKey()) {
-    event.target.value = listener.replace(event.target.value, shortcode, 'esto es mi replacement');
-    console.log('has encontrado un shortcode', listener.shortcode(), event.target.value);
-  }
+    listener.onKeyPressed(event);
+    console.log('key pressed 1', event);
+    if (listener.isTriggerKey()) {
+        listener.replace(event.target);
+        console.log('has encontrado un shortcode', listener.shortcode(), event.target.value);
+    }
 });
 
-chrome.runtime.sendMessage({ text: 'esto es desde cs' }, function (response) {
-  console.log('Response: ', response);
-  // parser.parseHtml();
+chrome.runtime.sendMessage({ text: 'esto es desde cs' }, function(response) {
+    console.log('Response: ', response);
+    // parser.parseHtml();
 });
--- src/core/page-listener.js
+++ src/core/page-listener.js
@@ -2,57 +2,58 @@ import parser from './parser';
 import settings from './settings';
 
 function PageListener() {
-  const triggerKey = '/';
-  let currentKey = {};
-  let stream = '';
+    const triggerKey = '/';
+    let currentKey = {};
+    let stream = '';
 
-  // const handleReplace = () => {};
+    // const handleReplace = () => {};
 
-  const onKeyPressed = (event) => {
-    // console.log('onKeyPressed', event.target.value);
-    currentKey = event;
-    if (event.key === ' ') {
-      stream = '';
-    }
-    if (!isTriggerKey()) {
-      stream += event.key;
-      stream = stream.trim();
-    }
+    const onKeyPressed = (event) => {
+        // console.log('onKeyPressed', event.target.value);
+        currentKey = event;
+        if (event.key === ' ') {
+            stream = '';
+        }
+        if (!isTriggerKey()) {
+            stream += event.key;
+            stream = stream.trim();
+        }
 
-    // handleReplace();
-    if (isTriggerKey()) {
-      const snippet = settings.getSnippetByShortcode(shortcode());
-      event.target.value = parser.parseHtmlToText(snippet.text);
-    }
-  };
+        // handleReplace();
+        if (isTriggerKey()) {
+            const snippet = settings.getSnippetByShortcode(shortcode());
+            event.target.value = parser.parseHtmlToText(snippet.text);
+        }
+    };
 
-  const isTriggerKey = () => {
-    if (currentKey.key === triggerKey) {
-      return true;
-    }
-    return false;
-  };
+    const isTriggerKey = () => {
+        if (currentKey.key === triggerKey) {
+            return true;
+        }
+        return false;
+    };
 
-  const shortcode = () => {
-    if (isTriggerKey()) {
-      return stream;
-    }
-    return '';
-  };
+    const shortcode = () => {
+        if (isTriggerKey()) {
+            return stream;
+        }
+        return '';
+    };
 
-  const replace = (text, shortcode, replacement) => {
-    console.log('going to replace', text, shortcode + triggerKey, replacement);
-    return text.replace(shortcode + triggerKey, replacement);
-  };
+    const replace = (element) => {
+        const snippet = settings.getSnippetByShortcode(shortcode());
+        console.log('going to replace', element.value, snippet);
+        return element.value.replace(shortcode() + triggerKey, snippet.text);
+    };
 
-  return {
-    onKeyPressed,
-    isTriggerKey,
-    shortcode,
-    replace,
-  };
+    return {
+        onKeyPressed,
+        isTriggerKey,
+        shortcode,
+        replace,
+    };
 }
 
 export default {
-  PageListener,
+    PageListener,
 };
